b5fcb34dd9f14c28c6c74e714441113e7ebd0707
Chris <christina@activo.nl>
2021-01-17 13:39:24 +0100

    Some css fixes

--- assets/app.js
+++ assets/app.js
@@ -199,15 +199,4 @@ bsCustomFileInput.init();
     //     items: 1
     // });
 
-    // Init AOS
-    function aos_init() {
-        AOS.init({
-            duration: 1000,
-            once: true
-        });
-    }
-    $(window).on('load', function() {
-        aos_init();
-    });
-
 })(jQuery);
